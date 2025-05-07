98e893673fb177433336be6b76357cfc636397fc
Mandeep Singh <panjatha.mandeep@gmail.com>
2025-05-07 11:54:02 +0530

    new repo

--- environment/dev/provider.tf
+++ environment/dev/provider.tf
@@ -5,13 +5,6 @@ terraform {
       version = "4.27.0"
     }
   }
-  backend "azurerm" {
-    resource_group_name  = "rg-tfstate"
-    storage_account_name = "ecomstorageaccountdev"
-    container_name       = "tfstate"
-    key                 = "dev.tfstate"
-    
-  }
 }
 
 provider "azurerm" {
